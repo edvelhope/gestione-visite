92818604e74099cd9e8ba1e7257982d5bf533578
colucciasimone <coluc@LAPTOP-TC6V3OK6>
2025-02-24 14:17:48 +0100

    elenco-dott

--- elenco-dott/cards-dott.js
+++ elenco-dott/cards-dott.js
@@ -1,15 +1,54 @@
-const cerca = document.getElementById('dott');
 
-cerca.addEventListener('focus', function() {
-  if (this.value === this.defaultValue) {
-    this.value = '';
-    this.style.color = 'black'; // Testo utente in nero
-  }
+/*const searchInput = document.getElementById('dott');
+const cards = document.querySelectorAll('.card');
+
+// Nascondi tutte le cards all'inizio
+cards.forEach(card => {
+  card.style.display = 'none';
 });
 
-cerca.addEventListener('blur', function() {
-  if (this.value === '') {
-    this.value = this.defaultValue;
-    this.style.color = 'gray'; // Testo predefinito in grigio
+searchInput.addEventListener('keyup', function(event) {
+  const cerca = searchInput.value.toLowerCase();
+
+  // Mostra tutte le cards se la barra di ricerca è vuota
+  if (cerca === '' || cerca===this.defaultValue) {
+    cards.forEach(card => {
+      card.style.display = 'block';
+    });
+    return;
+  }
+
+  cards.forEach(card => {
+    const cardTitle = card.dataset.title.toLowerCase();
+    if (cardTitle.includes(cerca)) {
+      card.style.display = 'block';
+    } else {
+      card.style.display = 'none';
+    }
+  });
+
+  if (event.key === 'Enter') {
+    // Qui puoi aggiungere del codice da eseguire quando l'utente preme Invio
+    console.log('Ricerca effettuata:', searchTerm);
   }
 });
+*/
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
