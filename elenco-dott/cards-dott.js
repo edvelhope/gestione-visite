ef00c82ed89c6c1b6e8e120c6ac5361d6beabc00
colucciasimone <coluc@LAPTOP-TC6V3OK6>
2025-02-25 17:49:13 +0100

    nuove features

--- elenco-dott/cards-dott.js
+++ elenco-dott/cards-dott.js
@@ -34,3 +34,18 @@ searchInput.addEventListener('keyup', function(event) {
 });
 */
 // Example starter JavaScript for disabling form submissions if there are invalid fields
+const nome = document.getElementById("nome");
+
+nome.addEventListener("invalid", function(event){
+  if (nome.validity.valueMissing) {
+    nome.setCustomValidity('Inserire un nome');
+  } else if (nome.validity.typeMismatch) {
+    nome.setCustomValidity('inserire un nome in formato corretto');
+  } else {
+    nome.setCustomValidity('');
+  }
+  return true;
+});
+nome.addEventListener("input", function(event){
+  nome.setCustomValidity('');
+});
