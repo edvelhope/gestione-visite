7d08a9516b9d0001ffef02c580f08a07e35095b8
colucciasimone <coluc@LAPTOP-TC6V3OK6>
2025-02-26 10:06:33 +0100

    spiegazioni

--- elenco-dott/cards-dott.js
+++ elenco-dott/cards-dott.js
@@ -1,39 +1,5 @@
 
-/*const searchInput = document.getElementById('dott');
-const cards = document.querySelectorAll('.card');
-
-// Nascondi tutte le cards all'inizio
-cards.forEach(card => {
-  card.style.display = 'none';
-});
-
-searchInput.addEventListener('keyup', function(event) {
-  const cerca = searchInput.value.toLowerCase();
-
-  // Mostra tutte le cards se la barra di ricerca è vuota
-  if (cerca === '' || cerca===this.defaultValue) {
-    cards.forEach(card => {
-      card.style.display = 'block';
-    });
-    return;
-  }
-
-  cards.forEach(card => {
-    const cardTitle = card.dataset.title.toLowerCase();
-    if (cardTitle.includes(cerca)) {
-      card.style.display = 'block';
-    } else {
-      card.style.display = 'none';
-    }
-  });
-
-  if (event.key === 'Enter') {
-    // Qui puoi aggiungere del codice da eseguire quando l'utente preme Invio
-    console.log('Ricerca effettuata:', searchTerm);
-  }
-});
-*/
-// Example starter JavaScript for disabling form submissions if there are invalid fields
+// controlli sulla barra di ricerca
 const nome = document.getElementById("nome");
 
 nome.addEventListener("invalid", function(event){
@@ -49,3 +15,8 @@ nome.addEventListener("invalid", function(event){
 nome.addEventListener("input", function(event){
   nome.setCustomValidity('');
 });
+
+const card = document.getElementsByClassName("card");
+function funzione (){
+
+}
